28f352435093791ae5f54ed7040e34c6b4077a35
Adharsh Kamath <adharshkamathr@gmail.com>
2020-05-13 17:15:04 +0530

    basic socket stuff

--- server.js
+++ server.js
@@ -1,21 +1,50 @@
 const express = require('express')();
 const server = require('http').createServer(express);
 const io = require('socket.io')(server);
-
-let players = [];
+const playersPerRoom = 3;
+var rooms = [null];
 
 io.on('connection', (socket) => {
+    var firstPlayer = "";
     console.log("Client connected w ID: " + socket.id);
+    if(rooms[rooms.length - 1] == null) {
+        rooms[rooms.length - 1] = [];
+        rooms[rooms.length - 1].push(socket.id);
+        firstPlayer = socket.id;
+        console.log(rooms);
+    }
+
+    else if(rooms[rooms.length - 1].length < playersPerRoom - 1) {
+        rooms[rooms.length - 1].push(socket.id);
+        firstPlayer = rooms[rooms.length - 1][0];
+        socket.join(firstPlayer);
+        console.log(rooms);
+    }
+
+    else if(rooms[rooms.length - 1].length == playersPerRoom - 1) {
+        rooms[rooms.length - 1].push(socket.id);
+        firstPlayer = rooms[rooms.length - 1][0];
+        socket.join(firstPlayer);
+        rooms.push(null);
+        console.log(rooms);
+    }
+
+    console.log(firstPlayer);
 
     socket.on('playerMoved', function(locationInfo) {
         console.log(locationInfo);
         socket.broadcast.emit('playerMoved', locationInfo, socket.id);
+        console.log(socket.adapter.rooms[socket.id].length);
     });
 
     socket.on('shotFired', function(bulletInfo) {
         console.log(bulletInfo);
         console.log(socket.id);
         socket.broadcast.emit('shotFired', bulletInfo, socket.id);
+    });
+
+    socket.on('disconnect', function() {
+        console.log(socket.id + " disconnected");
     });
 
 });
